cb04044d9e7743b567febeabb65eca395ad44425
chanv <chanvir.nijjar@gmail.com>
2022-05-25 22:49:13 -0400

    changes

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 superhero = pd.read_excel("data-output/sampledata.xlsx")
-
 #print(superhero.shape[0])
 
 superhero[['date','time']] = superhero['DateTime of birth'].str.split('T', n=1 ,expand=True)
