70858449a33e976cb5f37f7c34063778c28d3586
chanv <chanvir.nijjar@gmail.com>
2022-05-25 22:37:36 -0400

    split column, removed existing, moved data files into folder

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ superhero = pd.read_excel("sampledata.xlsx")
 
 #print(superhero.shape[0])
 
-superhero['DateTime of birth'].str.split('T',expand=True)
-superhero
-print(superhero)
+superhero[['date','time']] = superhero['DateTime of birth'].str.split('T', n=1 ,expand=True)
+superhero.drop('DateTime of birth',axis=1, inplace=True)
+
+
+print(superhero.head())
 
 superhero.to_excel('updated-data.xlsx')
 
