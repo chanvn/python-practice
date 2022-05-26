8e5cf23a300a494e577e883a37bde6fd591ec8ab
chanv <chanvir.nijjar@gmail.com>
2022-05-25 22:40:09 -0400

    fixed paths

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-superhero = pd.read_excel("sampledata.xlsx")
+superhero = pd.read_excel("data-output/sampledata.xlsx")
 
 #print(superhero.shape[0])
 
 superhero[['date','time']] = superhero['DateTime of birth'].str.split('T', n=1 ,expand=True)
-superhero.drop('DateTime of birth',axis=1, inplace=True)
+superhero.drop('DateTime of birth', axis=1, inplace=True)
 
 
 print(superhero.head())
 
-superhero.to_excel('updated-data.xlsx')
+superhero.to_excel('data-output/updated-data.xlsx')
 
